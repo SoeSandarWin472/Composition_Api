e83297d0199c742ca381034e6f5c9140bd882d6e
SoeSandarWin472 <soesandarwin47200@gmail.com>
2023-12-20 11:56:40 +0630

    composition api

---
+++ src/components/PostLists.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Post Lists</h1>
+  <div v-for="post in posts" :key="post.id">
+    <SinglePost :post="post"></SinglePost>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from 'vue'
+import SinglePost from './SinglePost'
+export default {
+  components: { SinglePost },
+props:['posts'],
+    setup(props){
+        onMounted(()=>console.log("mounted"));
+        onUnmounted(()=>console.log("unmunted"));
+        onUpdated(()=>console.log("updated"))
+    }
+}
+</script>
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <input type="text" v-model="search">
+    {{ search }}
+   <div v-for="name in filter" :key="name">
+{{ name }}
+   </div> -->
+   <div v-if="showPost">
+
+   <PostLists :posts="posts"></PostLists>
+   </div>
+   <button @click="showPost=!showPost">toggle post list</button>
+   <button @click="posts.pop()">delete</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostLists from '../components/PostLists'
+import { computed, ref } from 'vue'
+
+
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  components: { PostLists },
+  setup(){
+    // let search=ref("")
+    // let names=ref(["aungaung","kyawkyaw","susu","mgmg"])
+
+    // let filter=computed(()=>{
+    //   return names.value.filter(name=>{
+    //     return name.includes(search.value)
+    //   })
+    // })
+    // return {names,search,filter}
+  let showPost=ref(true)
+    let posts=ref([{title:"post title 1",body:"lorem ipsum",id:1},{title:"post title 2",body:"lorem ipsum",id:2}])
+    return {posts,showPost}
   }
 }
 </script>
